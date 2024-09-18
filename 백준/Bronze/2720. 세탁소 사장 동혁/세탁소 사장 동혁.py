a6875b48029f931c8a1ca93d33ffb42709527856
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2024-09-18 18:31:40 +0900

    Update 세탁소 사장 동혁.py

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,21 +1,36 @@
-coins = [25, 10, 5, 1]
-coin = 0
-res = [0 * 4 for _ in range(4)]
-tmp = 0
-T = int(input())
-arr = [0 * T for _ in range(T)]
-for i in range(T):
-    arr[i] = int(input())
+# coins = [25, 10, 5, 1]
+# coin = 0
+# res = [0 * 4 for _ in range(4)]
+# tmp = 0
+# T = int(input())
+# arr = [0 * T for _ in range(T)]
+# for i in range(T):
+#     arr[i] = int(input())
 
+# idx = 0
+# for i in range(T):
+#     coin = arr[i]
+#     for i in coins:
+#         res[idx] += coin // i
+#         coin %= i
+#         idx += 1
+#     for i in range(4):
+#         print(res[i], end=' ')
+#         idx = 0
+#     res = [0 * 4 for _ in range(4)] 
+#     print("")
+
+coins = [25, 10, 5, 1]
+res = [0 * 4 for _ in range(4)]
 idx = 0
+T = int(input())
 for i in range(T):
-    coin = arr[i]
-    for i in coins:
-        res[idx] += coin // i
-        coin %= i
+    coin = int(input())
+    for j in coins:
+        res[idx] += coin // j
+        coin %= j
         idx += 1
-    for i in range(4):
-        print(res[i], end=' ')
-        idx = 0
+    print(res[0], res[1], res[2], res[3], end=' ')
     res = [0 * 4 for _ in range(4)] 
-    print("")
+    idx = 0
+    print()
