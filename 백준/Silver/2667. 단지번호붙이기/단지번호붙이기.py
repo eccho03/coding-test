2b0a1a19b7b0cf9217edf394fa495ebc6ae8508b
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-02-26 00:33:27 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32504 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,36 +1,44 @@
-each = 0
-arr = []
-def dfs(x, y):
-    global each
-    if x <= -1 or x >= n or y <= -1 or y >= n:
-        return False
-    
-    # 해당 좌표 아직 방문 x
-    if graph[x][y] == 1:
-        graph[x][y] = 0 # 방문 처리
-        each += 1
-        dfs(x-1, y)
-        dfs(x, y-1)
-        dfs(x+1, y)
-        dfs(x, y+1)
-        return True
-    
-    return False
+house = []
+house_cnt = 0
+
+def dfs(x0, y0):
+    global house
+    global house_cnt
+
+    if x0 < 0 or y0 < 0 or x0 >= n or y0 >= n or graph[x0][y0] != 1:
+        return
+    visited[x0][y0] = True
+    house_cnt += 1
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    for i in range(4):
+        nx = x0 + dx[i]
+        ny = y0 + dy[i]
+
+        if nx < 0 or ny < 0 or nx >= n or ny >= n or graph[nx][ny] != 1:
+            continue
+        if not visited[nx][ny]:
+            dfs(nx, ny)
+    return house_cnt
 
 n = int(input())
-graph =[]
+total = 0
+graph = []
+visited = [[False] * n for _ in range(n)]
 for _ in range(n):
     graph.append(list(map(int, input())))
 
-result = 0
 for i in range(n):
     for j in range(n):
-        if dfs(i, j) == True:
-            result += 1
-            arr.append(each)
-            each = 0
+        house_cnt = 0
+        if graph[i][j] == 1 and not visited[i][j]:
+            total += 1
+            ans = dfs(i, j)
+            house.append(ans)
 
-print(result)
-arr.sort()
-for i in arr:
-    print(i)
+print(total)
+house.sort()
+for h in house:
+    print(h)
