de09a8636ff69a4eb9e0662d180d1df6cdd14d1e
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-02-26 13:04:58 +0900

    [Silver II] Title: 촌수계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,24 +1,27 @@
-res = -1
+chon = -1
 def dfs(v, cnt):
-    global res
+    global chon
     visited[v] = True
-    if v == p2:
-        res = cnt
+    if v == num2:
+        chon = cnt
         return
-    # print(v, end=' ')
+
     for i in graph[v]:
         if not visited[i]:
             dfs(i, cnt + 1)
 
-total = int(input())
-p1, p2 = map(int, input().split())
+n = int(input())
+num1, num2 = map(int, input().split())
 m = int(input())
-graph = [[] for _ in range(total + 1)]
-for i in range(m):
+
+# 번호 1부터 시작
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+
+for _ in range(m):
     x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
-    
-visited = [False] * (total + 1)
-dfs(p1, 0)
-print(res)
+
+dfs(num1, 0)
+print(chon)
