e62998c0e2fdfe84287bfd6171ab9c8b4ac2117c
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-04-05 17:39:37 +0900

    [Silver II] Title: DFS와 BFS, Time: 152 ms, Memory: 115340 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,41 +1,37 @@
-import sys
 from collections import deque
-def dfs(v):
-    visited[v] = True
-    print(v, end=' ')
-    for i in graph[v]:
+def dfs(start):
+    visited[start] = True
+    print(start, end=' ')
+    for i in graph[start]:
         if not visited[i]:
             dfs(i)
 
-def bfs(v):
-    queue = deque([v])
-    visited[v] = True
+def bfs(start):
+    q = deque([start])
+    visited[start] = True
 
-    while queue:
-        node = queue.popleft()
-        print(node, end=' ')
-        for i in graph[node]:
-            
+    while q:
+        v = q.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
             if not visited[i]:
-                queue.append(i)
+                q.append(i)
                 visited[i] = True
 
-n, m, v = map(int, sys.stdin.readline().split())
+n,m,v = map(int,input().split())
 
-graph = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
+visited = [False] * (n+1)
+graph = [[] * (n+1) for _ in range(n+1)]
 
 for _ in range(m):
-    node1, node2 = map(int, sys.stdin.readline().split())
-    graph[node1].append(node2)
-    graph[node2].append(node1)
+    n1, n2 = map(int,input().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1)
 
 for edge in graph:
     edge.sort()
     
 dfs(v)
-
-visited = [False] * (n + 1)
+visited = [False] * (n+1)
 print("")
-
 bfs(v)
