e312a9e77e8b69decfc32c8fb55204d11a4f7d36
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-04-06 18:45:37 +0900

    Title: 메이즈 러너, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250406/메이즈 러너/maze-runner.py
+++ 250406/메이즈 러너/maze-runner.py
@@ -1,103 +1,78 @@
 import sys
+sys.stdin = open('input.txt','r')
 input = sys.stdin.readline
 
+def cal_dis(ti,tj):
+    return abs(ti-ei) + abs(tj-ej)
+
 N, M, K = map(int, input().split())
-arr = [[10] * (N + 2)] + [[10] + list(map(int, input().split())) + [10] for _ in range(N)] + [[10] * (N + 2)]
-players = [tuple(map(int, input().split())) for _ in range(M)]
-exit_x, exit_y = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+for _ in range(M):
+    i, j = map(lambda x: int(x)-1, input().split()) # 1씩 감소시켜 받기
+    arr[i][j]-=1 # 같은 위치에 여러 명 있을 수도 있으므로
+
+ei,ej =map(lambda x:int(x)-1, input().split())
+arr[ei][ej] = -11 # 비상구 위치
+
+def find_exit(arr):
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == -11:
+                return i, j
+
+def find_square(arr):
+    mn = N
+    # [1] 비상구와 모든 사람 간의 가장 짧은 또는 가로 세러 거리 구하기
+    for i in range(N):
+        for j in range(N):
+            if -11 < arr[i][j] < 0: # 사람인 경우
+                mn = min(mn, max(abs(ei-i), abs(ej-j)))
+    # [2] 모두 순회하면서 길이 L인 정사각형에 비상구와 위치 구하기
+    for si in range(N-mn):
+        for sj in range(N-mn): # 가능한 모든 시작위치
+            if si<=ei<=si+mn and sj<=ej<=sj+mn: # 비상구가 포함된 위치
+                for i in range(si, si+mn+1):
+                    for j in range(sj, sj+mn+1):
+                        if -11 < arr[i][j] < 0:
+                            return si, sj, mn+1
 
 ans = 0
+cnt = M # 탈출 못한 생존자 수
+for _ in range(K): # K턴 반복
+    # [1] 모든 참가자 (동시에) 한 칸 이동(출구 최단 거리 방향)
+    narr = [x[:] for x in arr] # 좌변에 들어가는 것은 다 narr에다가 함
+    for i in range(N):
+        for j in range(N):
+            if -11 < arr[i][j] < 0:
+                cur_dis = cal_dis(i, j)
+                for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
+                    ni,nj = i+di, j+dj
+                    if 0<=ni<N and 0<=nj<N and arr[ni][nj]<=0:  # 벽이 없는 곳으로 이동 가능
+                        move_dis = cal_dis(ni, nj)  # 이동 위치에서 출구 거리 계산
+                        if cur_dis > move_dis:  # 더 짧아질 때에만 이동
+                            ans += arr[i][j] # 현재 좌표의 인원수가 이동함
+                            narr[i][j] -= arr[i][j] # 동시 이동이니까 0으로 하는 게 아닌 변위만큼!
+                            if arr[ni][nj] == -11: # 비상구라면 => 탈출
+                                cnt += arr[i][j]
+                            else:
+                                narr[ni][nj] += arr[i][j]
+                            break
+    arr = narr
+    if cnt == 0: break
 
-def dist(x, y):
-    return abs(x - exit_x) + abs(y - exit_y)
-
-# 가장 가까운 생존자가 포함된 가장 작은 정사각형 찾기
-def find_square():
-    min_d = float('inf')
-    cand = []
-
-    for x, y in players:
-        d = dist(x, y)
-        if d < min_d:
-            min_d = d
-            cand = [(x, y)]
-        elif d == min_d:
-            cand.append((x, y))
-
-    cand.sort()  # 위 -> 왼쪽 우선
-
-    for size in range(1, N + 1):
-        for i in range(1, N - size + 2):
-            for j in range(1, N - size + 2):
-                if not (i <= exit_x <= i + size - 1 and j <= exit_y <= j + size - 1):
-                    continue
-                for cx, cy in cand:
-                    if i <= cx <= i + size - 1 and j <= cy <= j + size - 1:
-                        return i, j, size
-    return -1, -1, -1
-
-def rotate(sx, sy, L):
-    global arr, exit_x, exit_y, players
-    temp = [[0] * L for _ in range(L)]
-
+    # [2] 미로 회전
+    # 시계방향 90도: 같은 크기 -> 좌상단 행,열 / 내구도 -1
+    si, sj, L = find_square(arr) # 비상구, 사람 포함 최소 정사각형
+    narr = [x[:] for x in arr]
     for i in range(L):
         for j in range(L):
-            temp[j][L - 1 - i] = arr[sx + i][sy + j]
+            narr[si+i][sj+j] = arr[si+(L-j-1)][sj+i]
+            if narr[si+i][sj+j] > 0:
+                narr[si+i][sj+j] -= 1 # 벽이면 회전시 1 감소
 
-    for i in range(L):
-        for j in range(L):
-            arr[sx + i][sy + j] = temp[i][j]
-            if arr[sx + i][sy + j] > 0:
-                arr[sx + i][sy + j] -= 1
+    arr = narr
+    # 회전으로 달라졌으므로 비상구 위치 저장
+    ei, ej = find_exit(arr)
 
-    # 출구 좌표 회전
-    if sx <= exit_x <= sx + L - 1 and sy <= exit_y <= sy + L - 1:
-        ox, oy = exit_x - sx, exit_y - sy
-        exit_x = sx + oy
-        exit_y = sy + (L - 1 - ox)
-
-    # 생존자 회전
-    new_players = []
-    for x, y in players:
-        if sx <= x <= sx + L - 1 and sy <= y <= sy + L - 1:
-            ox, oy = x - sx, y - sy
-            nx = sx + oy
-            ny = sy + (L - 1 - ox)
-            new_players.append((nx, ny))
-        else:
-            new_players.append((x, y))
-    players = new_players
-
-# 상하좌우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-for _ in range(K):
-    # [1] 생존자 이동
-    new_players = []
-    for x, y in players:
-        moved = False
-        for d in range(4):  # 상하좌우 우선순위
-            nx = x + dx[d]
-            ny = y + dy[d]
-            if arr[nx][ny] == 0 and dist(nx, ny) < dist(x, y):
-                new_players.append((nx, ny))
-                ans += 1
-                moved = True
-                break
-        if not moved:
-            new_players.append((x, y))
-    players = new_players
-
-    # [2] 이동 후 즉시 탈출 처리
-    players = [p for p in players if p != (exit_x, exit_y)]
-    if not players:
-        break
-
-    # [3] 회전
-    sx, sy, l = find_square()
-    if sx != -1:
-        rotate(sx, sy, l)
-
-print(ans)
-print(exit_x, exit_y)
+print(-ans)
+print(ei+1, ej+1)
