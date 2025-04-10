de2cde02703566a0934dbdb2dc5224401c2c8799
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-04-10 23:24:06 +0900

    Title: 왕실의 기사 대결, Time: 71ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250410/왕실의 기사 대결/royal-knight-duel.py
+++ 250410/왕실의 기사 대결/royal-knight-duel.py
@@ -1,49 +1,51 @@
+import sys
+sys.stdin = open('input.txt','r')
+
 di = [-1, 0, 1, 0]
 dj = [0, 1, 0, -1] # 상 우 하 좌
 
 from collections import deque
-def push_unit(num, dr):
+def push_unit(start, dr):
     q = deque()
-    pset = set() # 이동하는 기사번호를 저장
+    pset = set()
+    damage = [0]*(M+1)
 
-    q.append(num)
-    pset.add(num)
-
-    si,sj,sh,sw,sk = knight[num]
+    q.append(start)
+    pset.add(start)
 
     while q:
-        nidx = q.popleft()
-        ci, cj, h, w, k = knight[nidx]
+        cur = q.popleft()
+        ci,cj,h,w,k = knight[cur]
 
-        # 명령받은 방향 진행 / 벽이 아니면 / 겹치는 다른 조각이면 => 큐에 삽입
+        # 명령받은 방향진행, 벽이아니면, 겹치는 다른조각이면 => 큐에 삽입
         ni,nj = ci+di[dr], cj+dj[dr]
-
-        # 이동한 지점에서 데미지 / 벽에 대한 처리
-        for i in range(ni,ni+h):
-            for j in range(nj,nj+w):
-                if arr[i][j]==2: # 벽이면 => 모두 취소 (밀 수 없음)
-                    return
-                elif arr[i][j]==1 and (i,j)!=(si,sj):
-                    damage[nidx]+=1
+        for i in range(ni, ni+h):
+            for j in range(nj, nj+w):
+                if arr[i][j]==2:    return
+                if arr[i][j]==1 and cur!=start:
+                    damage[cur]+=1
 
         # 벽은 아니 / 모두 다 이동했을 때 데미지 처리를 해야 함 !! 그전에 삭제돼버리면 안 밀릴 수도 있기 때문에
         # 겹치는 다른 유닛 있는 경우 모두 큐에 추가
         for idx in knight:
-            if idx in pset: continue  # 이미 움직일 대상이면 체크할 필요 x
-            ti,tj,th,tw,tk = knight[idx]
+            if idx in pset: continue    # 이미 움직일 대상이면 체크할 필요없음
+
+            ti,tj,th,tw,tk=knight[idx]
+            # 겹치는 경우
             if ni<=ti+th-1 and ni+h-1>=ti and tj<=nj+w-1 and nj<=tj+tw-1:
                 q.append(idx)
                 pset.add(idx)
 
+    # 이동 중
     for idx in pset:
-        ki, kj, h, w, k = knight[idx]
-        # 체력보다 더 큰 데미지면 삭제 처리
-        if damage[idx] >= k:
+        si,sj,h,w,k = knight[idx]
+
+        if k<=damage[idx]:
             knight.pop(idx)
-        # 그렇지 않다면 이동
         else:
-            ni, nj = ki+di[dr], kj+dj[dr]
-            knight[idx] = [ni,nj,h,w,k - damage[idx]]
+            ni,nj=si+di[dr], sj+dj[dr]
+            knight[idx]=[ni,nj,h,w,k-damage[idx]]
+
 
 N, M, Q = map(int, input().split())
 arr = [[2]*(N+2)]+[[2]+list(map(int, input().split()))+[2] for _ in range(N)]+[[2]*(N+2)]
@@ -56,11 +58,8 @@ for idx in range(1, M+1):
     knight[idx] = [r,c,h,w,k]
     init_k[idx]=k
 
-    for i in range(r,r+h):
-        v[i][c:c+w] = [idx]*w # 디버거 동작 확인용
 order = [tuple(map(int, input().split())) for _ in range(Q)]
 
-ans=0
 for i, dr in order: # Q번에 걸친 왕의 명령
     if i not in knight: # 기사 목록에 없으면 아무런 반응x
         continue
