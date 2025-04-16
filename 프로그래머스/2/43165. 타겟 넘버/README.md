6865022d48c538edf97671d7ae140dbc912971a5
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-04-16 15:09:45 +0900

    [level 2] Title: 타겟 넘버, Time: 385.78 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 9.28 MB, 시간: 385.00 ms
+메모리: 9.29 MB, 시간: 385.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 16일 14:42:25
+2025년 04월 16일 15:09:43
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,21 +1,16 @@
 cnt = 0
-def dfs(numbers,val,target, N, size):
+def dfs(numbers, val, target, N, size):
     global cnt
-    #print(val)
-    if N==size-1:
-        if val==target:
-            cnt+=1
-            #print(cnt)
-        return cnt
+    if N == size - 1:
+        if val == target:
+            cnt += 1
+        return
+    dfs(numbers, val - numbers[N + 1], target, N + 1, size)
+    dfs(numbers, val + numbers[N + 1], target, N + 1, size)
 
-    dfs(numbers,val-numbers[N+1],target, N+1, size)
-    dfs(numbers,val+numbers[N+1],target, N+1, size)
-    
-    return cnt
-    
 def solution(numbers, target):
     global cnt
-    answer = 0
-    answer=dfs(numbers,numbers[0],target, 0, len(numbers))
-    answer=dfs(numbers,-numbers[0],target, 0, len(numbers))
-    return answer
+    cnt = 0 
+    dfs(numbers, numbers[0], target, 0, len(numbers))
+    dfs(numbers, -numbers[0], target, 0, len(numbers))
+    return cnt
