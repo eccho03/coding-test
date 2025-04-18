24bf5b635e155f8674bbe01111a42b743f49eb3d
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-04-18 14:03:44 +0900

    Update 문자열 나누기.py

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,27 +1,23 @@
 def solution(s):
     answer = 0
-    cnt1, cnt2 = 0, 0
-    tmp = ""
-    copy_s = s
-
-    while len(copy_s) > 0:
-        x = copy_s[0]
-        cnt1, cnt2 = 0, 0
-        tmp = ""
-
-        for i in range(len(copy_s)):
-            tmp += copy_s[i]
-            if copy_s[i] == x:
-                cnt1 += 1
+    cnt1,cnt2=0,0
+    tmp=""
+    while len(s)>0:
+        x = s[0]
+        
+        for i in range(len(s)):
+            tmp+=s[i]
+            if x==s[i]:
+                cnt1+=1
             else:
-                cnt2 += 1
-            
-            if cnt1 == cnt2:
-                answer += 1
-                copy_s = copy_s[i+1:]
+                cnt2+=1
+            if cnt1==cnt2:
+                answer+=1
+                cnt1,cnt2=0,0
+                s=s[i+1:]
                 break
         else:
-            answer += 1
+            answer+=1
             break
-
+            
     return answer
