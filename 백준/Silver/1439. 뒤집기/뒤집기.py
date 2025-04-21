8db872843a7f5593d8a4700997e00b9ed769eff9
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-04-21 23:49:11 +0900

    [Silver V] Title: 뒤집기, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,37 +1,16 @@
 s = str(input())
 
-sidx, eidx,tidx = -1,-1, -1
 start = False
 idx = 0
 cnt_0, cnt_1 = 0,0
-while True:
-    # print(idx)
-    if idx>=len(s)-1:
-        if s[eidx-1]!=s[eidx]:
-            if s[eidx]=="0":
-                cnt_0+=1
-            else:
-                cnt_1+=1
-        break
 
-    if s[idx]=="0":
-        cnt_0+=1
-        sidx,eidx = idx,idx
-        while True:
-            if s[idx]=="1" or idx>=len(s)-1:
-                break
-            idx += 1
-            eidx += 1
-        #print("0:",sidx, eidx)
+if s[0]=="0":
+    cnt_0+=1
+else:
+    cnt_1+=1
+for i in range(1,len(s)):
+    if s[i]!=s[i-1]:
+        if s[i]=="0":   cnt_0+=1
+        else:           cnt_1+=1
 
-    elif s[idx]=="1":
-        cnt_1 += 1
-        sidx, eidx = idx,idx
-        while True:
-            if s[idx]=="0" or idx>=len(s)-1:
-                break
-            idx+=1
-            eidx+=1
-        #print("1:",sidx, eidx)
-    idx = eidx
 print(min(cnt_0, cnt_1))
