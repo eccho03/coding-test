ea7f8303e453df4b5b8931fe2d5cad1445613758
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-06-01 22:58:21 +0900

    [Silver I] Title: 용돈 관리, Time: 3024 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.py
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.py
@@ -1,24 +1,26 @@
 N, M = map(int, input().split())
-days = list(int(input()) for _ in range(N))
+days = [int(input()) for _ in range(N)]
 
 start, end = max(days), sum(days)
-K = 0
+K = 0  # 최소 인출 금액
 
 while start <= end:
-    mid = (start+end)//2
+    mid = (start + end) // 2
 
-    total = 0
-    cnt = 0
+    budget = 0  # 현재 남은 금액
+    cnt = 1  # 최소 1번은 인출
 
-    for day in days:
-        if total < day:
-            total = mid
-            cnt += 1
-        total -= day
+    for today in days:
+        if budget + today > mid:
+            cnt += 1  # 인출
+            budget = today
+        else:
+            budget += today  # 예산에 오늘의 지출을 더함
 
-    if cnt>M:
-        start = mid+1
+    if cnt > M:
+        start = mid + 1
     else:
-        end = mid-1
+        end = mid - 1
         K = mid
+
 print(K)
