bfbacd2534ed1ac61728b9624742f1e54c0b8108
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-07-10 10:13:07 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 432 ms, Memory: 234292 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 def find_target(target):
     arr = [0]*N
     for i in range(N):
@@ -12,17 +15,27 @@ def sum_arr(arr):
 
     return prefix_arr
 
-S = input()
+S = input().strip()
 q = int(input())
 problems = [list(map(str, input().split())) for _ in range(q)]
 # print(problems)
+alpha = "abcdefghijklmnopqrstuvwxyz"
 
 N = len(S)
 
+alpha_cnt = []
+for i in range(len(alpha)):
+    arr = find_target(alpha[i])
+    alpha_cnt.append(arr)
+
+prefix_alpha_cnt = []
+for i in range(len(alpha_cnt)):
+    prefix_arr = sum_arr(alpha_cnt[i])
+    prefix_alpha_cnt.append(prefix_arr)
+
+# print(ord('c')-ord('a'))
 for target, l, r in problems:
     l, r = int(l), int(r)
-    arr = find_target(target)
-    prefix_arr = sum_arr(arr)
-    # print(arr)
-    # print(prefix_arr)
-    print(prefix_arr[r+1]-prefix_arr[l])
+    #print(S[l:r+1])
+    idx = ord(target)-ord('a')
+    sys.stdout.write(str(prefix_alpha_cnt[idx][r+1]-prefix_alpha_cnt[idx][l])+'\n')
