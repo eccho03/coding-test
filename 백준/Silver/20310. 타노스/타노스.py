ad168c0a256bd3936be538a6dda87c4dbda521dd
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-07-28 09:50:26 +0900

    [Silver III] Title: 타노스, Time: 40 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,14 +1,22 @@
 S = input().rstrip()
 
-cnt_0, cnt_1 = 0, 0
+cnt_0, cnt_1 = S.count('0')//2, S.count('1')//2
 
+tmp = []
 for ch in S:
-    if ch == '1':
-        cnt_1 += 1
-    elif ch == '0':
-        cnt_0 += 1
+    # 앞에서부터 1 제거
+    if ch == '1' and cnt_1 > 0:
+        cnt_1 -= 1
+    else:
+        tmp.append(ch)
 
-cnt_0, cnt_1 = cnt_0//2, cnt_1//2
-# print(cnt_0, cnt_1)
+ans = []
+# print(tmp)
+for ch in reversed(tmp):
+    # 뒤에서부터 0 제거
+    if ch == '0' and cnt_0 > 0:
+        cnt_0 -= 1
+    else:
+        ans.append(ch)
 
-print('0'*cnt_0+'1'*cnt_1)
+print(''.join(reversed(ans)))
