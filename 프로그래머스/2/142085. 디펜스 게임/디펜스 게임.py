4d62c6e82281a7095ddd7ce80d2810df228e1815
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-07-31 09:26:12 +0900

    [level 2] Title: 디펜스 게임, Time: 6751.52 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -1,31 +1,30 @@
 def solution(n, k, enemy):
     answer = 0
-    rounds = len(enemy)
     
-    start, end = 0, rounds
+    start, end = 0, len(enemy)
     
-    def check(mid, n, k):
-        cur = enemy[:mid]
-        target = sorted(cur, reverse=True)
-        # print(enemy[:mid])
-        # print(target)
-
-        for i in range(mid):
+    def is_alive(target, n):
+        sorted_target = sorted(target, reverse = True)
+        
+        for i in range(len(target)):
             if i < k:
-                continue  # 무적권 사용
-            if target[i] > n:
+                continue    # 무족권 사용
+            if n < sorted_target[i]:
                 return False
-            n -= target[i]
+            
+            n-=sorted_target[i]
+        
         return True
-      
+            
     
     while start <= end:
         mid = (start+end)//2
         
-        if check(mid, n, k):
-            start = mid+1
+        if is_alive(enemy[:mid], n):
+            start = mid+1 # 살아남았으면 더 많은 라운드를 살아남을 수 있는지 확인
             answer = mid
+            
         else:
             end = mid-1
-
+            
     return answer
