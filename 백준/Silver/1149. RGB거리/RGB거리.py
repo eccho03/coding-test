e70c220d39d02f453fd90ad35c5811ce9eb5b5ef
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-08-06 09:31:11 +0900

    [Silver I] Title: RGB거리, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,16 +1,12 @@
-n = int(input())
-house = []
-d = [[0, 0, 0] for _ in range(n)]
-for i in range(n):
-    r, g, b = map(int, input().rstrip().split())
-    house.append([r, g, b])
+N = int(input())
+colors = [list(map(int, input().split())) for _ in range(N)]
 
-d[0] = house[0]
+dp = [[0]*3 for _ in range(N)]
+dp[0] = colors[0]
 
-for i in range(1, n):
-    d[i][0] = house[i][0] + min(d[i-1][1], d[i-1][2])
-    d[i][1] = house[i][1] + min(d[i-1][0], d[i-1][2])
-    d[i][2]= house[i][2] + min(d[i-1][0], d[i-1][1])
+for i in range(1, N):
+    dp[i][0] = min(dp[i-1][1] + colors[i][0], dp[i-1][2] + colors[i][0])
+    dp[i][1] = min(dp[i-1][0] + colors[i][1], dp[i-1][2] + colors[i][1])
+    dp[i][2] = min(dp[i-1][0] + colors[i][2], dp[i-1][1] + colors[i][2])
 
-ans = min(d[i][0], d[i][1], d[i][2])
-print(ans)
+print(min(dp[N-1]))
