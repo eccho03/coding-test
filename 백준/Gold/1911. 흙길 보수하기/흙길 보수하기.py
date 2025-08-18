e5a5c159197aa3c0e99fcf175207fea7d9c7a3ad
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-08-18 10:51:10 +0900

    [Gold V] Title: 흙길 보수하기, Time: 296 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
@@ -1,23 +1,22 @@
-import math
-
 N, L = map(int, input().split())
 info = [list(map(int, input().split())) for _ in range(N)]
 info.sort()
 ans = 0
-idx = 0
+idx = 0 # 현재까지 온 위치
 
 for start, end in info:
-    # print(start, end)
-
     if idx>start:
         start=idx
 
-    if idx>=end:
-        continue # 커버 가능
+    if end<=idx:
+        continue # 새로 안 덮어도 커버 가능
 
-    cnt = math.ceil((end-start)/L)
-    ans += cnt   # 널빤지 개수
-    idx = start + L*cnt
-    # print(idx)
+    if (end-start)%L==0:
+        cnt = (end-start)//L
+    else:
+        cnt = (end-start)//L+1
+    ans += cnt
+    idx = start+cnt*L
+
 
 print(ans)
