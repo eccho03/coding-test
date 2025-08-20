3bc40db691f16acb5f4124cb446c60766d6563f4
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-08-20 22:58:53 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 197.11 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -1,25 +1,41 @@
 import math
 def solution(arrayA, arrayB):
     ans_lst = []
+    answer = 0
     
-    # 최대공약수 구하기
     def gcd_list(arr):
         g = arr[0]
         for x in arr[1:]:
             g = math.gcd(g, x)
         return g
     
+    def get_divisors(num):
+        divs = set()
+        for i in range(1, int(math.sqrt(num)) + 1):
+            if num % i == 0:
+                divs.add(i)
+                divs.add(num // i)
+        return sorted(divs, reverse=True)
+    
     mx_mul_A = gcd_list(arrayA)
     mx_mul_B = gcd_list(arrayB)
+    mx_muls_A = get_divisors(mx_mul_A)
+    mx_muls_B = get_divisors(mx_mul_B)
     
-    # print("mula",mx_mul_A)
-    # print("mulb",mx_mul_B)
+    # print("mula",mx_muls_A)
+    # print("mulb",mx_muls_B)
     
-    if all(j%mx_mul_A!=0 for j in arrayB):
-        ans_lst.append(mx_mul_A)
-    if all(j%mx_mul_B!=0 for j in arrayA):
-        ans_lst.append(mx_mul_B)
-
+    if len(mx_muls_A)!=1:
+        for num in mx_muls_A:
+            if all(num>=j or j%num!=0 for j in arrayB):
+                ans_lst.append(num)
+                break
+    if len(mx_muls_B)!=1:        
+        for num in mx_muls_B:
+            if all(num>=j or j%num!=0 for j in arrayA):
+                ans_lst.append(num)
+                break
+    
     if len(ans_lst)==0:
         answer=0
     else:
