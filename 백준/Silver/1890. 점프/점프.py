db2e04105428fda48ed6b851e83694f393364814
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-08-22 09:26:35 +0900

    [Silver I] Title: 점프, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,27 @@
+def dfs(ci, cj):
+    global cnt
+    if (ci,cj)==(ei,ej):
+        return 1
+    if v[ci][cj]!=-1:
+        return v[ci][cj]
+
+    v[ci][cj]=0
+    dist = arr[ci][cj]
+    if dist==0:
+        return 0
+    for di,dj in directions:
+        ni,nj = ci+di*arr[ci][cj], cj+dj*arr[ci][cj]
+        if 0<=ni<N and 0<=nj<N:
+            v[ci][cj] += dfs(ni,nj)
+    return v[ci][cj]
+
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+si,sj, ei,ej = 0,0, N-1,N-1
+
+directions = [(1,0),(0,1)] # 하, 우
+v = [[-1]*N for _ in range(N)]
+ans = dfs(si, sj)
+print(ans)
