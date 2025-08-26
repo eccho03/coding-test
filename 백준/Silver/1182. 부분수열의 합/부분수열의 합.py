9cc09dc624ac7573bf6160593260e254fb5e4782
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-08-26 10:16:44 +0900

    [Silver II] Title: 부분수열의 합, Time: 212 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,18 +1,19 @@
-def dfs(idx, sum):
+def dfs(idx, cur_sum):
     global cnt
-    if idx == N:
+    if idx>=N:
+        if cur_sum==S:
+            cnt+=1
         return
 
-    sum += num[idx]
-    if sum==S:
-        cnt+=1
-    dfs(idx+1, sum)
-    dfs(idx+1, sum-num[idx])
+    dfs(idx+1, cur_sum+arr[idx])
+    dfs(idx+1, cur_sum)
 
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
 
-
-N,S=map(int,input().split())
-num = list(map(int,input().split()))
-cnt = 0
+cnt=0
 dfs(0, 0)
+
+if S==0:
+    cnt-=1
 print(cnt)
