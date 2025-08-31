541c9583398f165526f4abc40e4a99848656e54a
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-08-31 14:53:48 +0900

    [Silver III] Title: 근손실, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.py
+++ 백준/Silver/18429. 근손실/근손실.py
@@ -1,24 +1,29 @@
-from itertools import permutations
+# 하루가 지날때마다 중량 K씩 감소
+# N개의 운동키트
+# 1~N일차 항상 중량 500 이상이어야 함
 
-def use_kit(kits):
-    muscles = 500
+def check(orders):
+    cur_weight = 500
     for i in range(N):
-        muscles -= K
-        muscles += kits[i]
-
-        # print(muscles)
-
-        if muscles < 500:
+        cur_kit = kit[orders[i]-1]
+        cur_weight -= K
+        cur_weight += cur_kit
+        #print(cur_kit)
+        if cur_weight < 500:
             return False
-
     return True
 
+
+
+from itertools import permutations
+
 N, K = map(int, input().split())
-A = list(map(int, input().split()))
-ans = 0
+kit = list(map(int, input().split()))
+days = [i for i in range(1,N+1)]
 
-for order in permutations(A, N):
-    if use_kit(order):
-        ans+=1
+cnt=0
+for i in permutations(days, N):
+    if check(i):
+        cnt+=1
 
-print(ans)
+print(cnt)
