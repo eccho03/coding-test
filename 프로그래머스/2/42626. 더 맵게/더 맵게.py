53baa4e7b859cdde00537528bef45e9bb53b4d0e
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-09-10 10:12:31 +0900

    [level 2] Title: 더 맵게, Time: 1680.85 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -2,25 +2,29 @@ import heapq
 
 def solution(scoville, K):
     answer = 0
-    N = len(scoville)
     heapq.heapify(scoville)
-    
+    # print(scoville)
+    cnt=0
     while len(scoville)>1:
-        mn_scov_first = heapq.heappop(scoville)
-        mn_scov_second = heapq.heappop(scoville)
-        
-        if mn_scov_first >= K or answer>=N-1:
+        cur = heapq.heappop(scoville)
+        if cur < K:
+            nxt = heapq.heappop(scoville)
+            mix = cur + (nxt*2)
+            # print(nxt, mix)
+            heapq.heappush(scoville, mix)
+        else:
             break
-        
-        mixed_scov = mn_scov_first + (mn_scov_second*2)
-        heapq.heappush(scoville, mixed_scov)
-            
-        answer+=1
         # print(scoville)
+        cnt+=1
     
-    for i in range(len(scoville)):
-        if scoville[i]<K:
-            return -1
+    if scoville:
+        mn_scoville = heapq.heappop(scoville)
+        if mn_scoville>=K:
+            answer = cnt
+        else:
+            answer = -1
+    else:
+        answer = cnt
         
     
     return answer
