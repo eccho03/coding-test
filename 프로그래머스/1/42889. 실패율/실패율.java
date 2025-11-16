c7c0fafa89e3f5aa021ee5b5c2daf7208403f157
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2025-11-16 20:13:47 +0900

    [level 1] Title: 실패율, Time: 457.92 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,11 +1,9 @@
 import java.util.*;
-
 class Solution {
-    
     static class Stage {
         float ratio;
         int stage;
-
+        
         Stage(float ratio, int stage) {
             this.ratio = ratio;
             this.stage = stage;
@@ -13,33 +11,27 @@ class Solution {
     }
     
     public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
         List<Stage> ratio = new ArrayList<>();
-        int [] answer = new int[N];
         
         for (int i=1; i<=N; i++) {
-            int all_cnt=0;
-            int ing_cnt=0;
-            for (int j=0; j<stages.length; j++){
-                if (stages[j]>=i) {
-                    all_cnt++;
-                }
-                if (stages[j]==i) {
-                    ing_cnt++;
-                }
+            int completed=0;
+            int doing=0;
+            for (int j=0; j<stages.length; j++) {
+                if (stages[j]>=i) completed++;
+                if (stages[j]==i) doing++;
             }
-            // System.out.println(all_cnt+" "+ing_cnt);
-            float failRatio = (all_cnt == 0) ? 0 : (float) ing_cnt / all_cnt;
-            ratio.add(new Stage(failRatio, i));
-            
+            if (completed==0) ratio.add(new Stage(0, i));
+            else ratio.add(new Stage((float)doing/completed, i));
         }
         
-        Collections.sort(ratio, (a, b)-> {
+        ratio.sort((a, b) -> {
             if (a.ratio==b.ratio) return a.stage-b.stage;
             return Float.compare(b.ratio, a.ratio);
         });
-            
+        
         for (int i=0; i<N; i++) {
-            answer[i] = ratio.get(i).stage;
+            answer[i]=ratio.get(i).stage;
         }
         
         return answer;
