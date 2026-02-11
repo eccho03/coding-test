d2378e6307ad85711f7357d25a66e35092d31e02
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2026-02-11 20:25:49 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.10 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,24 +1,20 @@
+import java.util.*;
 class Solution {
     public String solution(String s) {
         StringBuilder answer = new StringBuilder();
-        int idx=0;
-        for (int i=0; i<s.length(); i++) {
-            Character ch = s.charAt(i);
+        int idx = 0;
+        
+        for (char ch: s.toCharArray()) {
             if (ch==' ') {
                 answer.append(' ');
-                idx=0;
+                idx = 0;
             }
             else {
-                if (idx%2==0) {
-                    answer.append(Character.toUpperCase(ch));
-                }
-                else {
-                    answer.append(Character.toLowerCase(ch));
-                }
+                if (idx%2==0) answer.append(Character.toUpperCase(ch));
+                else answer.append(Character.toLowerCase(ch));
                 idx++;
             }
         }
-        
         
         return answer.toString();
     }
