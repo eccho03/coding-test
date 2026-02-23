8e49c1347e769453e84fcdefc26dc5d2913ba864
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2026-02-23 21:22:31 +0900

    [level 2] Title: 무인도 여행, Time: 10.17 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/README.md
+++ 프로그래머스/2/154540. 무인도 여행/README.md
@@ -1,10 +1,10 @@
 # [level 2] 무인도 여행 - 154540 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154540?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154540) 
 
 ### 성능 요약
 
-메모리: 90.5 MB, 시간: 12.17 ms
+메모리: 74.9 MB, 시간: 10.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 16일 20:50:10
+2026년 02월 23일 21:22:28
 
 ### 문제 설명
 
--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,77 +1,66 @@
 import java.util.*;
+
 class Solution {
-    static int N;
-    static int M;
     
     static class Point {
         int x;
         int y;
-        
         Point(int x, int y) {
-            this.x=x;
-            this.y=y;
+            this.x = x;
+            this.y = y;
         }
     }
     
-    public int[] solution(String[] maps) {
-        List<Integer> answer = new ArrayList<>();
+    static int bfs(String[] maps, boolean[][] visited, Point start) {
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+        int cnt = maps[start.x].charAt(start.y) -'0';
+        int N = maps.length;
+        int M = maps[0].length();
         
-        N = maps.length;
-        M = maps[0].length();
-        boolean[][] visited = new boolean[N][M];
-        int[][] board = new int[N][M];
-        
-        for (int i=0; i<N; i++) {
-            String tmp = maps[i];
-            for (int j=0; j<M; j++) {
-                if (tmp.charAt(j)=='X') board[i][j]=0;
-                else board[i][j] = Integer.parseInt(String.valueOf(tmp.charAt(j)));
-            }
-        }
-        
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<M; j++) {
-                if (!visited[i][j] && board[i][j]!=0) {
-                    int tmp = bfs(visited, board, new Point(i, j));
-                    answer.add(tmp);
-                }
-            }
-        }
-        
-        if (answer.size()>0) Collections.sort(answer);
-        else answer.add(-1);
-        
-        return answer.stream().mapToInt(i->i).toArray();
-    
-    }
-    
-    static int bfs(boolean[][] visited, int[][] board, Point start) {
-        int[] dx = {0,0,-1,1};
-        int[] dy = {-1,1,0,0};
-        int stayDays = 0;
-        
-        Queue<Point> q = new LinkedList<Point>();
+        Queue<Point> q = new LinkedList<>();
         q.add(start);
         visited[start.x][start.y]=true;
-        stayDays += board[start.x][start.y];
         
         while (!q.isEmpty()) {
             Point cur = q.poll();
             
             for (int i=0; i<4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
+                int ni = cur.x + dx[i];
+                int nj = cur.y + dy[i];
                 
-                if (nx<0||nx>=N||ny<0||ny>=M) continue;
-                if (visited[nx][ny]) continue;
-                if (board[nx][ny]==0) continue;
+                if (ni<0 || ni>=N || nj<0 || nj>=M) continue;
+                if (visited[ni][nj]) continue;
+                if (maps[ni].charAt(nj)=='X') continue;
                 
-                q.add(new Point(nx, ny));
-                visited[nx][ny]=true;
-                stayDays += board[nx][ny];
+                q.add(new Point(ni, nj));
+                visited[ni][nj]=true;
+                
+                //System.out.println(maps[ni].charAt(nj));
+                cnt+=maps[ni].charAt(nj) -'0';
+            } 
+        }
+        return cnt;
+    }
+    
+    public int[] solution(String[] maps) {
+        List<Integer> answer = new ArrayList<>();
+        int N = maps.length;
+        int M = maps[0].length();
+        
+        boolean[][] visited = new boolean[N][M];
+        
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<M; j++) {
+                if (!visited[i][j] && maps[i].charAt(j)!='X') {
+                    int tmp = bfs(maps, visited, new Point(i, j));
+                    visited[i][j]=true;
+                    if (tmp!=0) answer.add(tmp);
+                }
             }
         }
-        
-        return stayDays;
+        if (answer.isEmpty()) return new int[] {-1};
+        Collections.sort(answer);
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
