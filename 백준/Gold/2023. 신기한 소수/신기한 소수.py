03a0bf95346b43317417b5222d2c54e4c01ada3d
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2026-03-24 13:59:43 +0900

    [Gold V] Title: 신기한 소수, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,27 +1,28 @@
 import math
 
-N = int(input())
-
-def check(num):
-    if num==1:
+def dfs(num, idx):
+    if not sosu(int(num[:idx+1])):
         return False
+    if idx==N-1:
+        print(num)
+        return True
+
+    dfs(num+'1', idx+1)
+    dfs(num+'2', idx+1)
+    dfs(num+'3', idx+1)
+    dfs(num+'5', idx+1)
+    dfs(num+'7', idx+1)
+    dfs(num+'9', idx+1)
+
+def sosu(num):
+    if num==1: return False
     for i in range(2, int(math.sqrt(num))+1):
-        if num%i==0:
-            return False
+        if num%i==0: return False
     return True
 
-def dfs(num, idx):
+N = int(input())
 
-    if idx==N:
-        print(num)
-        return
-
-    # print(num[:idx+1])
-    for d in range(10):
-        nxt = num*10+d
-
-        if check(nxt)==True:
-            dfs(nxt, idx+1)
-
-for i in (2,3,5,7):
-    dfs(i, 1)
+dfs('2', 0)
+dfs('3', 0)
+dfs('5', 0)
+dfs('7', 0)
