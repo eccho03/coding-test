687100f9b1c699d5c5f6045688d8a346346183a0
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2026-03-24 14:38:14 +0900

    [Silver I] Title: 경로 찾기, Time: 112 ms, Memory: 35084 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,34 +1,48 @@
-INF = float('inf')
-def floyd_warshall():
-    dist = [[INF]*N for _ in range(N)]
+from collections import deque
 
-    for i in range(N):
-        for j in range(N):
-            dist[i][j] = arr[i][j]
+def bfs(start, end):
+    q = deque()
+    v = [0] * N
 
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                if dist[i][j] > dist[i][k] + dist[k][j]:
-                    dist[i][j] = dist[i][k] + dist[k][j]
+    q.append(start)
+    v[start] = 1
 
-    return dist
+    while q:
+        cur = q.popleft()
+
+        for nxt in graph[cur]:
+            if not v[nxt]:
+                if nxt == end:
+                    return True
+                v[nxt] = 1
+                q.append(nxt)
+
+            elif nxt == end:
+                return True
+
+    return False
+
+def print_arr(arr):
+    for row in arr:
+        print(*row)
+    print()
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
-for i in range(N):
-    for j in range(N):
-        if arr[i][j]==0:
-            arr[i][j]=INF
 
-dist = floyd_warshall()
-ans = [[0]*N for _ in range(N)]
-for i in range(N):
-    for j in range(N):
-        if 0<dist[i][j]<INF:
-            ans[i][j]=1
-        else:
-            ans[i][j]=0
+graph = [[] for _ in range(N)]
 
 for i in range(N):
-    print(*ans[i])
+    for j in range(N):
+        if arr[i][j]:
+            graph[i].append(j)
+
+# print(graph)
+
+answer = [[0]*N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if bfs(i, j):
+            answer[i][j]=1
+
+print_arr(answer)
