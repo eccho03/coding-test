bee02bb85d3a7cd68e5a1af3fb21378659aaac21
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2026-03-27 15:25:15 +0900

    [Silver I] Title: 귀여운 라이언, Time: 536 ms, Memory: 51932 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.py
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.py
@@ -1,22 +1,21 @@
 N, K = map(int, input().split())
 arr = list(map(int, input().split()))
 
-left, right = 0, N-1
+left, right = 0, 0
 cnt = 0
 answer = float('inf')
-for right in range(N):
-    if arr[right]==1:
-        cnt+=1
 
-    while cnt>=K:
-        answer = min(answer, right-left+1)
+while right < N:
+    if arr[right] == 1:
+        cnt += 1
 
-        if arr[left]==1:
-            cnt-=1
-        left+=1
+    while cnt >= K:
+        answer = min(answer, right - left + 1)
 
-if answer==float('inf'):
-    print(-1)
-else:
-    print(answer)
+        if arr[left] == 1:
+            cnt -= 1
+        left += 1
 
+    right += 1
+
+print(answer if answer != float('inf') else -1)
