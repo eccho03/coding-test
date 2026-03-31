7958365143222b83c025a7e9b8f835632b401109
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2026-03-31 16:00:08 +0900

    [level 2] Title: 올바른 괄호, Time: 19.12 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i)=='(')
+                stack.push(s.charAt(i));
+            else {
+                if (!stack.isEmpty() && stack.peek()=='(') {
+                    stack.pop();
+                }
+                else return false;
+            }
+            // System.out.println(stack);
+        }
+        
+        answer = stack.size()==0? true : false;
+
+        return answer;
+    }
+}
