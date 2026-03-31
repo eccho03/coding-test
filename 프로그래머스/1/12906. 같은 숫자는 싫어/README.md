4dc23652406de04ab60b9b82b8fdb218f766cbac
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2026-03-31 15:39:01 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.78 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 119 MB, 시간: 34.55 ms
+메모리: 118 MB, 시간: 39.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 18일 21:16:28
+2026년 03월 31일 15:38:58
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,21 +2,16 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        int[] answer = {};
-        int N = arr.length;
+        // int[] answer = {};
+        ArrayList<Integer> answer = new ArrayList<>();
         
-        ArrayList<Integer> numList = new ArrayList<Integer>();
-        numList.add(arr[0]);
-        for (int i=1; i<N; i++) {
-            if (arr[i]==arr[i-1]) continue;
-            numList.add(arr[i]);
+        answer.add(arr[0]);
+        for (int i=1; i<arr.length; i++) {
+            if (arr[i-1]!=arr[i])
+                answer.add(arr[i]);
+            else continue;
         }
-        
-        // for (int num: numList){
-        //     System.out.println(num);
-        // }
-        
-        // list to array
-        return numList.stream().mapToInt(i->i).toArray();
+
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
