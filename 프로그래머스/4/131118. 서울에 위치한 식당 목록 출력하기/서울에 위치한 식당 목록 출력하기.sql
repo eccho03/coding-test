6ff141aa1fc24eec1bde5f6bce652c14ea5764b7
Eunchae Cho <108281892+eccho03@users.noreply.github.com>
2026-05-08 12:06:06 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -0,0 +1,9 @@
+select 
+    I.REST_ID, I.REST_NAME, I.FOOD_TYPE, I.FAVORITES as FAVORITES, I.ADDRESS, round(avg(R.REVIEW_SCORE), 2) as SCORE
+    
+from REST_INFO I
+join REST_REVIEW R
+on I.REST_ID=R.REST_ID
+where I.ADDRESS like '서울%'
+group by I.REST_ID, I.REST_NAME, I.FOOD_TYPE, I.FAVORITES, I.ADDRESS
+order by SCORE desc, I.FAVORITES desc
